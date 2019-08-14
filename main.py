fbbc78f9eb0fee23c8250d2274fa57fa2d6651e4
neXXus <sagar77ghimire@gmail.com>
2019-08-14 17:12:28 +0545

    Added feature for image segmentation or clustering

--- main.py
+++ main.py
@@ -60,6 +60,47 @@ def upload_files():
             return redirect(request.url)
     return render_template('imageclustering.html')
 
+@app.route('/numericclustering', methods=['GET', 'POST'])
+def upload_numeric_cluster():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            if filename == "kmean-config.config":
+                file.save(
+                    os.path.join(
+                        '/home/nexxus/Python/Final_Year/static/files/kmeans',
+                        filename))
+                CONFIG_kMEAN='/home/nexxus/Python/Final_Year/static/files/kmeans/'+filename
+                from kmeans import main
+                main(CONFIG_kMEAN)
+
+            elif filename=="meanshift-config.config":
+                file.save(
+                    os.path.join(
+                        '/home/nexxus/Python/Final_Year/static/files/meanshift',
+                        filename))
+                config_meanshift ='/home/nexxus/Python/Final_Year/static/files/meanshift/'+filename
+                from Meanshift import main
+                main(config_meanshift)
+            else:
+                file.save(
+                    os.path.join(
+                        '/home/nexxus/Python/Final_Year/static/csv',
+                        filename))
+
+            return redirect(request.url)
+    return render_template('numericclustering.html')   
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -102,27 +143,6 @@ def upload_file():
     #         return redirect(request.url)
     return render_template('index.html')
 
-# def upload_meanshift():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-
-#         meanshift = request.files['meanshift']
-#         # if user does not select file, browser also
-#         # submit an empty part without filename
-#         if meanshift.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if meanshift and allowed_file(meanshift.filename):
-#             filename = secure_filename(meanshift.filename)
-#             meanshift.save(
-#                 os.path.join(
-#                     '/home/sagar/Python/test/Final_Year/images/meanshift',
-#                     filename))
-
-#             return redirect(request.url)
-#     return render_template('index.html')
-
-
 
 @app.route('/upload/<filename>')
 def send_image(filename):
@@ -138,7 +158,7 @@ def kmeans():
 
 
 
-@app.route('/<abc>')
+@app.route('/abc/<abc>')
 def abc(abc):
     return send_from_directory("images/meanshift", abc)
 
@@ -173,5 +193,48 @@ def delete_shift():
     return render_template('meanshift.html')
 
 
+@app.route('/kimage/<kimage>')
+def image(kimage):
+    return send_from_directory("images/kmeanimg", kimage)
+
+#kmean iamge
+@app.route('/kmeanImage')
+def kmeanImage():
+    image_nam = os.listdir('images/kmeanimg')
+    image_nam=natsort.natsorted(image_nam)
+    print(image_nam)
+    return render_template("KmeanImage.html", image_nam=image_nam)
+
+@app.route('/KmeanImage', methods=['POST', 'GET'])
+def delete_shif():
+    if request.method == 'POST':
+        mydir = '/home/nexxus/Python/Final_Year/images/kmeanimg'
+        for file in os.listdir(mydir):
+            if file.endswith('.jpg'):
+                os.remove(os.path.join(mydir, file))
+                return redirect(request.url)
+    return render_template('KmeanImage.html')
+
+@app.route('/shiftimage/<shiftimage>')
+def meanshiftimage(shiftimage):
+    return send_from_directory("images/meanshiftimg", shiftimage)
+
+#meanshift image
+@app.route('/Meanshiftimg')
+def MeanshiftImage():
+    image_nama = os.listdir('images/meanshiftimg')
+    image_nama=natsort.natsorted(image_nama)
+    print(image_nama)
+    return render_template("Meanshiftimg.html", image_nama=image_nama)
+
+@app.route('/Meanshiftimg', methods=['POST', 'GET'])
+def delete_shifs():
+    if request.method == 'POST':
+        mydir = '/home/nexxus/Python/Final_Year/images/meanshiftimage'
+        for file in os.listdir(mydir):
+            if file.endswith('.jpg'):
+                os.remove(os.path.join(mydir, file))
+                return redirect(request.url)
+    return render_template('Meanshiftimg.html')
 if __name__ == '__main__':
     app.run(debug=True)
